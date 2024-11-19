0f16f91acbd7ecd6e734e047ea36885c3152ff2e
Sofia Robles <robles@wustl.edu>
2024-11-18 18:27:53 -0600

    setting up html structure, universal css, attempting to link api in javascript

---
+++ src/script.js
@@ -0,0 +1,15 @@
+const apiKey = 'qanT1ugOgF6T6gDeRO881G8tKMTbvWl1ldGWkAed'
+const apiUrl = 'https://api.nasa.gov/planetary/apod'
+const divClass = document.querySelector('api-data');
+
+document.getElementById('fetchData').addEventListener('click', async () => {
+    try {
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) throw new Error('Failed to fetch data');
+        const data = await response.json();
+        } catch (error) {
+            console.error(error);
+            document.getElementById('result').textContent = 'Error fetching data';
+        }
+    });
