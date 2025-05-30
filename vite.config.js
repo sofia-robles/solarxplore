13fbcb261ff4ffcae06577f6d7d39d52856b85c5
Sofia Robles <robles@wustl.edu>
2025-05-30 18:51:10 -0500

    redoing vite config for images through git

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/solarxplore/',
+  base: '/solarxplore/', 
   plugins: [react()],
 })
